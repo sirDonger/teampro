265395bfa34fb531b313123507bde3d9e0438a69
arsalan.gohar@avest-group.com <arsalan.gohar@avest-group.com>
2020-03-20 21:22:31 +0500

    3.1.3 #3 Category - @Arsalan

--- src/app/category/components/categories/categories.component.ts
+++ src/app/category/components/categories/categories.component.ts
@@ -1,3 +1,5 @@
+import { Categories } from './../../../shared/_models/category.model';
+import { ApiService } from './../../../shared/_services/api.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,88 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesComponent implements OnInit {
 
-  constructor() { }
+  openMenu: string[] = ['home_services'];
+  categoriesList: Categories = [];
+  categoriesData = {
+    'title': 'Home Service',
+    'images': [
+        'http://s8.staging-host.com/xpbid/images/category3.png',
+        'http://s8.staging-host.com/xpbid/images/category2.png',
+        'http://s8.staging-host.com/xpbid/images/category1.png',
+      ],
+    'description': `<p>
+    Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+    accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae
+    vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+    voluptas sit aspernatur aut odit aut fugit, sed quia
+    consequuntur magni dolores eos qui ratione voluptatem sequi
+    nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor
+    sit amet, consectetur, adipisci velit, sed quia non numquam eius
+    modi tempora incidunt ut labore et dolore magnam aliquam quaerat
+    voluptatem. Ut enim ad minima veniam, quis nostrum
+    exercitationem ullam corporis suscipit laboriosam.
+  </p>
+  <p>
+  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+  blanditiis praesentium voluptatum deleniti atque corrupti quos
+  dolores et quas molestias excepturi sint occaecati cupiditate
+  non provident, similique sunt in culpa qui officia deserunt
+  mollitia animi, id est laborum et dolorum fuga. Et harum quidem
+  rerum facilis est et expedita distinctio. Nam libero tempore,
+  cum soluta nobis est eligendi optio cumque nihil impedit quo
+  minus id quod maxime placeat facere possimus, omnis voluptas
+  assumenda est.
+  </p>
+  <p>
+  <em>Temporibus autem quibusdam et aut officiis debitis aut rerum
+    necessitatibus saepe eveniet ut et voluptates repudiandae sint
+    et molestiae non recusandae. Itaque earum rerum hic tenetur a
+    sapiente delectus, ut aut reiciendis voluptatibus maiores
+    alias consequatur aut perferendis doloribus asperiores
+    repellat."
+  </em>
+</p>
+<ul class="list_items cate-list">
+<li>Sed ut perspiciatis unde omnis</li>
+<li>Iste natus error sit voluptatem</li>
+<li>Accusantium doloremque</li>
+<li>Laudantium, totam rem aperiam,</li>
+<li>Eaque ipsa quae ab</li>
+</ul>
+<ul class="list_items cate-list">
+  <li>Sed ut perspiciatis unde omnis</li>
+  <li>Iste natus error sit voluptatem</li>
+  <li>Accusantium doloremque</li>
+  <li>Laudantium, totam rem aperiam,</li>
+  <li>Eaque ipsa quae ab</li>
+</ul>
+<p>
+  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+  accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+  quae ab illo inventore veritatis et quasi architecto beatae
+  vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia
+  voluptas sit aspernatur aut odit aut fugit, sed quia
+  consequuntur magni dolores eos qui ratione voluptatem sequi
+  nesciunt.
+</p>
+`
+};
+
+  constructor(private apiService: ApiService) { }
 
   ngOnInit() {
+
+    this.apiService.getAllCategories(false).subscribe(response =>  this.categoriesList = response);
+    
+  }
+
+  toggleMenu(menu_slug: string){
+    if (this.openMenu.indexOf(menu_slug) === -1 ){
+      this.openMenu.push(menu_slug);
+    }else{
+      this.openMenu.splice(this.openMenu.indexOf(menu_slug), 1);
+    }
   }
 
 }
